dc07d028d87297627f97c7bbc7cb5b30bd5d59e8
PartedMagic <74645668+partedMagic@users.noreply.github.com>
2023-04-15 16:05:02 +0100

    Update omni_oakland_64.mk

--- omni_oakland_64.mk
+++ omni_oakland_64.mk
@@ -25,9 +25,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # Inherit from oakland_64 device
 $(call inherit-product, device/tcl/oakland_64/device.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
+# Inherit some common Omni stuff. | Fix for PitchBlack RP 10
+$(call inherit-product, vendor/pv/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := oakland_64
