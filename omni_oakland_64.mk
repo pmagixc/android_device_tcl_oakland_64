cc3e9422949ba8db53061d76ee9806f173d7c6cd
scopiwolf <74645668+scopiwolf@users.noreply.github.com>
2023-11-17 19:29:14 +0200

    Update omni_oakland_64.mk

--- omni_oakland_64.mk
+++ omni_oakland_64.mk
@@ -1,8 +1,3 @@
-#
-# Copyright (C) 2020 The Android Open Source Project
-# Copyright (C) 2020 The TWRP Open Source Project
-# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
-#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -15,20 +10,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
-# Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
-# Inherit from oakland_64 device
 $(call inherit-product, device/tcl/oakland_64/device.mk)
-
-# Inherit some common Omni stuff. | Fix for PitchBlack RP 10
 $(call inherit-product, vendor/pb/config/common.mk)
 
-# Device identifier. This must come after all inclusions
+
 PRODUCT_DEVICE := oakland_64
 PRODUCT_NAME := omni_oakland_64
 PRODUCT_BRAND := alps
